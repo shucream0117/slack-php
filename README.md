c4f8a0da74e42e22d65697fb65788ebc90c0e382
shucream0117 <shucream0117@users.noreply.github.com>
2022-07-26 03:44:22 +0900

    新方式のAPIに対応

--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 
 ## Requirement
 
-- PHP 7.4.x
+- PHP >= 7.4.x
+- Incoming WebHook URL via Slack Apps
+  - https://api.slack.com/messaging/webhooks
 
 ## Installation
 
 via composer.
 
 ```console
-$ composer require shucream0117/slack-php:2.0
+$ composer require shucream0117/slack-php:3.0
 ```
 
 ## Usage
@@ -17,15 +19,9 @@ $ composer require shucream0117/slack-php:2.0
 ```php
 $slack = new Slack('http://your-slack-incoming-webhook-url');
 
-// specify channel(optional)
-$slack->setChannel('somewhere');
+// send a message with mention
+$slack->send('this message will be posted']);
 
-// set user name(optional)
-$slack->setUserName('someone');
-
-// set icon emoji
-$slack->setIconEmoji('ok_woman');
-
-// send a message
-$slack->send('this message will be posted');
+// send a message with mention
+$slack->send('mention!!', ['@HOGEFUGA']);
 ```
--- src/Slack.php
+++ src/Slack.php
@@ -16,42 +16,22 @@ class Slack
         $this->httpClient = $client ?: new Client();
     }
 
-    public function setChannel(string $channel): self
-    {
-        $this->params['channel'] = $channel;
-        return $this;
-    }
-
-    public function setUserName(string $userName): self
-    {
-        $this->params['username'] = $userName;
-        return $this;
-    }
-
-    public function setIconEmoji(string $iconEmoji): self
-    {
-        $this->params['icon_emoji'] = $iconEmoji;
-        return $this;
-    }
-
-    /**
-     * @see https://api.slack.com/changelog/2017-09-the-one-about-usernames
-     * @deprecated
-     */
-    public function setLinkNames(bool $linkNames): self
-    {
-        $this->params['link_names'] = (int)$linkNames;
-        return $this;
-    }
-
     /**
      * @param string $message
+     * @param string[] $mentionMemberIds ユーザー名ではないSlackの内部ID的なもの。
+     * @return void
      */
-    public function send(string $message): void
+    public function send(string $message, array $mentionMemberIds = []): void
     {
+        $mention = '';
+        foreach ($mentionMemberIds as $id) {
+            $mention .= "<@{$id}> ";
+        }
         $this->httpClient->post($this->incomingWebHookUrl, [
             'form_params' => [
-                'payload' => json_encode(array_merge($this->params, ['text' => $message])),
+                'payload' => json_encode(array_merge($this->params, [
+                    'text' => $mention . $message
+                ])),
             ],
         ]);
     }
